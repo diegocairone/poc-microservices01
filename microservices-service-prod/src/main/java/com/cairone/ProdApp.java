2241ae0a46c4fb96157c8fee8612dcdcb4bc8e51
Diego Cairone <diegocairone@gmail.com>
2021-07-08 17:35:19 -0300

    Add Eureka client and server

--- microservices-service-prod/src/main/java/com/cairone/ProdApp.java
+++ microservices-service-prod/src/main/java/com/cairone/ProdApp.java
@@ -2,6 +2,7 @@ package com.cairone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class ProdApp {
 

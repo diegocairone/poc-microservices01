03deae1e6ad24689103c05f2f5db73cd41603539
Diego Cairone <diegocairone@gmail.com>
2021-07-08 11:24:11 -0300

    Service for multiplication math operation

---
+++ microservices-service-prod/src/main/java/com/cairone/ProdApp.java
@@ -0,0 +1,35 @@
+package com.cairone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+public class ProdApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdApp.class, args);
+	}
+
+	@GetMapping("/api/sum")
+	public ResponseEntity<Response> productory(long a, long b) {
+	    Long result = a * b;
+	    log.info("Productory of {} * {} = {}", a, b, result);
+	    return ResponseEntity.ok(new Response(result));
+	}
+	
+	@Data
+	@AllArgsConstructor
+	public static class Response {
+	    
+	    private long result;
+	}
+}

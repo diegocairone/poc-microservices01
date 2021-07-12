19c588541a6bdd300cba593ea3131a1a491a1a69
Diego Cairone <diegocairone@gmail.com>
2021-07-12 12:05:50 -0300

    Routing

--- microservices-edge-server/src/main/java/com/cairone/EdgeServerApp.java
+++ microservices-edge-server/src/main/java/com/cairone/EdgeServerApp.java
@@ -1,7 +1,13 @@
 package com.cairone;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -25,18 +31,37 @@ public class EdgeServerApp {
         return builder.routes()
                 .route("sum-service", r -> r
                         .path("/sum/**")
-                        .filters(f -> f.rewritePath(
-                                "/sum", 
-                                "/api/sum"))
+                        .filters(f -> f.rewritePath("/sum(?<segment>/?.*)", "$\\{segment}"))
                         .uri("lb://SUM-SERVICE")
                 )
                 .route("prod-service", r -> r
                         .path("/prod/**")
-                        .filters(f -> f.rewritePath(
-                                "/prod", 
-                                "/api/prod"))
+                        .filters(f -> f.rewritePath("/prod(?<segment>/?.*)", "$\\{segment}"))
                         .uri("lb://PROD-SERVICE")
                 )
+                /*
+                .route("openapi", r -> r
+                        .path("/v3/api-docs/prod/**")
+                        .filters(f -> f.rewritePath(
+                                "/v3/api-docs/prod/prod", 
+                                "/prod/v3/api-docs/**"))
+                        .uri("http://localhost:7000")
+                )*/
                 .build();
+    }
+	
+
+    //@Autowired
+    RouteDefinitionLocator locator;
+
+    //@Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service", "");
+            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+        });
+        return groups;
     }
 }
